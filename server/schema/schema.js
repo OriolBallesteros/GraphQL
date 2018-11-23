4af6c9a28dcca0d539cf9d5b96eb8c5763a4b5a5
Manel Ballesteros <ballesterosoriol@gmail.com>
2018-11-23 21:35:51 +0100

    mutation - addBooks

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -114,6 +114,23 @@ const Mutation = new GraphQLObjectType({
 
                 return author.save();                  //--> the incredible mongoose lets us access the DDBB as simple as this line
             }
+        },
+        addBook: {
+            type: BookType,
+            args: {
+                name:  {type: GraphQLString},
+                genre: {type: GraphQLString},
+                authorId: {type: GraphQLID}
+            },
+            resolve(parent, args){
+                let book = new Book({
+                    name: args.name,
+                    genre: args.genre,
+                    authorId: args.authorId
+                });
+
+                return book.save();
+            }
         }
     }
 });
